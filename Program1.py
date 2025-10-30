0efb3ccbe100a8ec983d2d91dd41cbb4d3f2b4b0
Jewelianamartinez <jmart819@bruinmail.slcc.edu>
2025-10-29 18:30:05 -0600

    Initial program 1 upload

---
+++ Program1.py
@@ -0,0 +1,15 @@
+# Name: Jeweliana Martinez
+# Assigment: Program1
+# I earned +2 in class
+
+# 1. Use the print command to show the output:
+print(" @..@")
+print("(----)")
+print("(>__<)")
+print("^^~~^^")
+
+# 2. Include in the program using Input user to read variables for US state and Capital
+# from the keyboard, and print the information according to the following format:
+State = input("what state do you live in?")
+Capital = input("what capital of the state?")
+print("the capital of" + State + "is" + Capital)
